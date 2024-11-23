2555bb4274279cae2f120122ff85f9949377ef63
warun7 <varun.g.ug23@plaksha.edu.in>
2024-11-23 23:55:29 +0530

    updated favicon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
+  server: {
+    port: 5173
+  }
 });
